87009aef348acce91c8d65cc2d99718b73b7c812
Anh Trieu <anh.trieu@icloud.com>
2018-02-25 15:12:11 -0800

    final non-oop commit

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
 const argv = require('yargs').argv
-const moves = ['rock', 'paper', 'scissors']
 const humanMove = argv.move
-// const computerPick =
 const computerMove = (arr => {
   return arr[Math.floor(Math.random() * 3)]
-})(moves)
+})(['rock', 'paper', 'scissors'])
 
 const play = (human, computer) => {
   if ((human === 'rock') && (computer === 'scissors')) {
@@ -26,7 +24,6 @@ const winner = (() => {
   }
 })()
 
-console.log(`Playing a game of Roshambo against the computer.`)
-console.log(`Player plays ${humanMove}!`)
-console.log(`Computer plays ${computerMove}!`)
-console.log(`~${winner} wins.~`)
+const logArr = [`Playing a game of Roshambo against the computer.`, `Player plays ${humanMove}!`, `Computer plays ${computerMove}!`, `~${winner} wins.~`]
+
+logArr.forEach(line => console.log(line))
